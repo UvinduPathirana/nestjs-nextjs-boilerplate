1c4a423cd8842774b28e62563c55fffdb55a5304
Harindu Fonseka <harindulakshitha525@gmail.com>
2024-06-13 09:23:10 +0530

    signin and singun form and protected route

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function DashboardPage() {
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Welcome to the dashboard!</p>
+        </div>
+    );
+}
