00c6812ee8d8e15071bc8c88b98c16959314c417
uvindu pathirana <upathirana112@gmail.com>
2024-06-14 15:53:41 +0530

    #we-004 Implemented controller routes and service methods for getting data from weatherapicom

--- backend/src/weatherapi/weatherapi.module.ts
+++ backend/src/weatherapi/weatherapi.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WeatherapiController } from './weatherapi.controller';
 import { WeatherapiService } from './weatherapi.service';
+import { HttpModule } from '@nestjs/axios';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [HttpModule, AuthModule],
   controllers: [WeatherapiController],
   providers: [WeatherapiService]
 })
--- backend/src/weatherapi/weatherapi.service.ts
+++ backend/src/weatherapi/weatherapi.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class WeatherapiService {}
+export class WeatherapiService {
+
+    constructor(private http: HttpService) {}
+    
+    async getWeather(city: string) {
+        const response = await this.http.get(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${city}&days=1&aqi=no&alerts=no`).pipe(
+            map(response => response.data)
+        ).toPromise();
+        return response;
+    }
+}
