3ab0a4ac72ad02e92696b45996c0d78c88401a87
uvindu pathirana <upathirana112@gmail.com>
2024-06-14 16:03:17 +0530

    #we-004 error handling implemented, getting weather api key from config without accessing directly env variables

--- backend/src/weatherapi/weatherapi.service.ts
+++ backend/src/weatherapi/weatherapi.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { map } from 'rxjs/operators';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class WeatherapiService {
+  constructor(
+    private http: HttpService,
+    private configService: ConfigService,
+  ) {}
 
-    constructor(private http: HttpService) {}
-    
-    async getWeather(city: string) {
-        const response = await this.http.get(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${city}&days=1&aqi=no&alerts=no`).pipe(
-            map(response => response.data)
-        ).toPromise();
+  async getWeather(city: string) {
+    const url = `http://api.weatherapi.com/v1/forecast.json`;
+    const params = {
+      key: this.configService.get('WEATHER_API_KEY'),
+      q: city,
+      days: 1,
+      aqi: 'no',
+      alerts: 'no',
+    };
+
+    try {
+        const response = await this.http.get(url, {params}).pipe(map(response => response.data)).toPromise();
         return response;
-    }
+    } catch (err) {
+        console.error(err);
+        throw new Error('Failed to get weather data');
+      }
+  }
 }
