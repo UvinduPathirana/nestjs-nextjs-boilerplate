5323adaf1052b7cb82700d2d4eb5ec0b36e6bb0d
Harindu Fonseka <harindulakshitha525@gmail.com>
2024-06-18 10:40:28 +0530

    dashboard with components for each element

---
+++ frontend/components/dashboard/add-city-search.tsx
@@ -0,0 +1,16 @@
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
+
+export default function SearchCity() {
+    return (
+
+    <div className="relative ml-auto flex-1 md:grow-0">
+        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+        <Input
+            type="search"
+            placeholder="Search..."
+            className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[405px]"
+        />
+    </div>
+    );
+}
