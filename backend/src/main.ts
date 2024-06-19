375a8e37081edbb27ef3441bfe2a0be1e8e75024
uvindu pathirana <upathirana112@gmail.com>
2024-06-19 11:38:55 +0530

    #we-008 Created docker files for frontend and backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,15 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.intercepter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: 'http://localhost:3002',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-    },
-  });  
+  const app = await NestFactory.create(AppModule);  
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
