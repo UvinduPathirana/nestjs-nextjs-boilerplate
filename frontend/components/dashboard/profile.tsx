5ff531bc0d5a9b0933c10b99061b124dd397f95f
Harindu Fonseka <harindulakshitha525@gmail.com>
2024-06-19 17:32:57 +0530

    #we-007 alert box before deleting a city,  toaster when add a city and set the first city on the list automatically when loading

--- frontend/components/dashboard/profile.tsx
+++ frontend/components/dashboard/profile.tsx
@@ -7,52 +7,51 @@ import avatarimage from "@/public/avatar.jpg"
 
 export default function Profile() {
 
-    function handleLogout() {
-        fetch("/api/auth/logout", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-        .then(response => {
-          if (response.ok) {
-            console.log('Logged out successfully');
-            //redirect to signin page
-            window.location.href = '/signin';
-            // Add any additional logic needed after logout, e.g., redirecting the user
-          } else {
-            console.error('Failed to log out');
-          }
-        })
-        .catch(error => {
-          console.error('An error occurred:', error);
-        });
-      }
-      
-      
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button
-                    variant="outline"
-                    size="icon"
-                    className="overflow-hidden rounded-full"
-                >
-                    <Image
-                        src={avatarimage.src}
-                        width={36}
-                        height={36}
-                        alt="Avatar"
-                        className="overflow-hidden rounded-full"
-                    />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Edit</DropdownMenuItem>
-                <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    );
+  function handleLogout() {
+    fetch("/api/auth/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Logged out successfully');
+          window.location.href = '/signin';
+        }
+        else {
+          console.error('Failed to log out');
+        }
+      })
+      .catch(error => {
+        console.error('An error occurred:', error);
+      });
+  }
+
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="overflow-hidden rounded-full"
+        >
+          <Image
+            src={avatarimage.src}
+            width={36}
+            height={36}
+            alt="Avatar"
+            className="overflow-hidden rounded-full"
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Edit</DropdownMenuItem>
+        <DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
